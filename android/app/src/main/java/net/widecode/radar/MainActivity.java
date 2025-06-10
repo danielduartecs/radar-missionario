bffc7c591a8cc9a8dc2c0832e8ca0d0542d68f17
Daniel Duarte <danielduarte@gmail.com>
2025-06-10 09:54:30 -0300

    Adiciona configuração do Capacitor e estrutura inicial do projeto Android, incluindo arquivos de configuração, ícones e layouts para o aplicativo "Radar Missionário".

---
+++ android/app/src/main/java/net/widecode/radar/MainActivity.java
@@ -0,0 +1,5 @@
+package net.widecode.radar;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'net.widecode.radar',
+  appName: 'Radar Missionário',
+  webDir: 'dist',
+};
+
+export default config;
